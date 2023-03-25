280ec022717a046e5217a41c4b1f3e97e73a74ca
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-26 04:27:07 +0900

    Feat: 카카오 로그인 토큰 발급 받기까지 완료

--- src/pages/Third/Third.jsx
+++ src/pages/Third/Third.jsx
@@ -1,14 +1,50 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { setUserInfo } from '../../redux/features/userSlice';
 
+export const kakaoLoginConfig = {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+  },
+};
+
 function Third() {
+  const navigate = useNavigate();
   const { activeUser } = useSelector((state) => state.userInfo);
   console.log('activeUser', activeUser);
   const dispatch = useDispatch();
 
   let code = new URL(window.location.href).searchParams.get('code');
   console.log('code', code);
+
+  const tokenData = {
+    grant_type: 'authorization_code',
+    client_id: `${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
+    redirect_uri: 'http://localhost:5173/third',
+    code,
+  };
+  const params = new URLSearchParams(tokenData);
+
+  const getToken = async () => {
+    try {
+      const res = await axios.post(
+        'https://kauth.kakao.com/oauth/token',
+        params.toString(),
+        kakaoLoginConfig
+      );
+      console.log('res', res);
+    } catch (error) {
+      console.log('소셜로그인 에러', error);
+      // window.alert('로그인에 실패하였습니다.');
+      // navigate('/');
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  }, [code]);
 
   useEffect(() => {
     dispatch(setUserInfo(code));
