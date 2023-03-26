a6bb7e6021cf5d3ed902244a8cd86f63ef596849
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-26 22:12:35 +0900

    Feat: 카카오 로그인 구현중... 백엔드가 필요하다...

--- src/pages/Third/Third.jsx
+++ src/pages/Third/Third.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import React, { useEffect } from 'react';
+import * as S from './Third.style';
 import { useCookies } from 'react-cookie';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -8,43 +8,56 @@ import {
   useGetUserTokenMutation,
 } from '../../redux/features/kakaoLogin';
 import { setUserInfo } from '../../redux/features/userSlice';
+import { message, Spin } from 'antd';
 
 function Third() {
   const [, setCookie] = useCookies();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const { activeUser } = useSelector((state) => state.userInfo);
   console.log('activeUser', activeUser);
   const dispatch = useDispatch();
 
   const [
     getToken,
-    { data: tokenData, error: getTokenError, isSuccess: getTokenSuccess },
-  ] = useGetUserTokenMutation();
-  const { data: tokenInfoData, error: getTokenInfoError } = useGetUserTokenInfoQuery(
-    tokenData?.access_token,
     {
-      skip: !tokenData?.access_token,
-    }
-  );
-
+      data: tokenData,
+      isLoading: getTokenLoading,
+      error: getTokenError,
+      isSuccess: getTokenSuccess,
+    },
+  ] = useGetUserTokenMutation();
+  const {
+    data: tokenInfoData,
+    isLoading: getTokenInfoLoading,
+    error: getTokenInfoError,
+  } = useGetUserTokenInfoQuery(tokenData?.access_token, {
+    skip: !tokenData?.access_token,
+  });
+  console.log('tokenData!!!!!!!!!!!!!!', tokenData);
   let code = new URL(window.location.href).searchParams.get('code');
   console.log('code', code);
 
-  const onSuccess = (isFirst, accessToken) => {
-    setCookie('airoverflow', accessToken, {
-      path: '/',
-      sameSite: 'lax',
-      secure: true,
-    });
-    if (isFirst) navigate('/second');
-    else navigate('/');
-  };
+  // useEffect(() => {
+  //   if(getTokenSuccess) {
 
-  const onError = () => {
-    message
-      .error('로그인에 문제가 발생했습니다.\n잠시 후 다시 시도해 주세요 :(')
-      .then(() => navigate('/'));
-  };
+  //   }
+  // })
+
+  // const onSuccess = (isFirst, accessToken) => {
+  //   setCookie('airoverflow', accessToken, {
+  //     path: '/',
+  //     sameSite: 'lax',
+  //     secure: true,
+  //   });
+  //   if (isFirst) navigate('/second');
+  //   else navigate('/');
+  // };
+
+  // const onError = () => {
+  //   message
+  //     .error('로그인에 문제가 발생했습니다.\n잠시 후 다시 시도해 주세요 :(')
+  //     .then(() => navigate('/'));
+  // };
 
   useEffect(() => {
     if (code) {
@@ -53,73 +66,43 @@ function Third() {
   }, [code, getToken]);
 
   useEffect(() => {
-    if (tokenData?.access_token) {
-      localStorage.setItem('access_token', JSON.stringify(tokenData.access_token));
-      console.log('tokenInfoData', tokenInfoData);
+    if (getTokenSuccess) {
+      if (tokenData?.access_token) {
+        const { access_token, id_token } = tokenData;
+        localStorage.setItem('access_token', JSON.stringify({ access_token, id_token }));
+        setCookie('airoverflow', id_token, {
+          path: '/',
+          sameSite: 'lax',
+          secure: true,
+        });
+
+        message
+          .success('로그인에 성공했습니다. 원을 클릭해서 즐겨찾기에 저장해보세요.')
+          .then(() => navigate('/first'));
+        // navigate('/first');
+      }
     }
   }, [tokenData]);
+
+  console.log('tokenInfoData', tokenInfoData);
+
+  useEffect(() => {
+    if (getTokenError || getTokenInfoError) {
+      message
+        .error('로그인에 문제가 발생했습니다.\n잠시 후 다시 시도해 주세요 :(')
+        .then(() => navigate('/'));
+    }
+  });
 
   useEffect(() => {
     dispatch(setUserInfo(code));
   }, []);
 
-  return <div>Third</div>;
+  return (
+    <S.Container>
+      {(getTokenInfoLoading || getTokenLoading) && <Spin size="large" />}
+    </S.Container>
+  );
 }
 
 export default Third;
-
-// export const kakaoLoginConfig = {
-//   headers: {
-//     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-//   },
-// };
-
-// const tokenData = {
-//   grant_type: 'authorization_code',
-//   client_id: `${import.meta.env.VITE_KAKAO_REST_API_KEY}`,
-//   redirect_uri: 'http://localhost:5173/third',
-//   code,
-// };
-// const params = new URLSearchParams(tokenData);
-
-// const getToken = async () => {
-//   try {
-//     const res = await axios.post(
-//       'https://kauth.kakao.com/oauth/token',
-//       params.toString(),
-//       kakaoLoginConfig
-//     );
-//     localStorage.setItem('access_token', JSON.stringify(res?.data?.access_token));
-//     console.log('res', res);
-//   } catch (error) {
-//     console.log('소셜로그인 에러', error);
-//     // window.alert('로그인에 실패하였습니다.');
-//     // navigate('/');
-//   }
-// };
-
-// const access_token = JSON.parse(localStorage.getItem('access_token')) || null;
-// const getTokenInfo = async () => {
-//   try {
-//     const res = await axios.get('https://kapi.kakao.com/v1/user/access_token_info', {
-//       headers: {
-//         Authorization: `Bearer ${access_token}`,
-//       },
-//     });
-//     console.log('토큰 정보!!!!', res);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// useEffect(() => {
-//   getTokenInfo();
-// }, [access_token]);
-
-// useEffect(() => {
-//   getToken();
-// }, [code]);
-
-// useEffect(() => {
-//   dispatch(setUserInfo(code));
-// }, []);
