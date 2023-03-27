8f6fc692385eb43b648603ee33088bc4c34daab6
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-28 01:45:23 +0900

    Fix: protected route 순수함수로 수정

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { message } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import { Navigate } from 'react-router-dom';
 
@@ -9,9 +9,15 @@ export default function ProtectedRoute({ children }) {
   const access_token = cookie?.airoverflow?.access_token;
   console.log('access_token', cookie?.airoverflow?.access_token);
 
+  useEffect(() => {
+    if (!access_token) {
+      message.info('즐겨찾기 페이지는 로그인 후 이용이 가능합니다.');
+    }
+  }, [access_token]);
+
   if (!access_token) {
-    message.info('즐겨찾기 페이지는 로그인 후 이용이 가능합니다.');
     return <Navigate to="/" replace />;
   }
+
   return children;
 }
